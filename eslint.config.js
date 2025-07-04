92c705cf36851fb159cbab6b45dc6d4c6c45621c
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-04 23:53:25 +0900

    test: lint-staged 작동 확인

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// eslint.config.js
+export default [
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+];
