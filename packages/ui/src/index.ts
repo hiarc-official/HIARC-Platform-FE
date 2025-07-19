08cd23735e251e7b5d92c9b6822d1b7fb96893fb
NOGUEN <nhg1113@icloud.com>
2025-07-20 01:46:21 +0900

    feat: title 컴포넌트 구현 #1
    - title 컴포넌트 구현
    - sm, lg 사이즈만 구현

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,4 +2,5 @@ export * from './components/button';
 export * from './components/card';
 export * from './components/input';
 export * from './components/label';
+export * from './components/title';
 export * from './lib/utils';
