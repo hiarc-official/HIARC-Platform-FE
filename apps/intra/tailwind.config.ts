d8bbd9ef61b66b9def5264fd957985a2469b3cd1
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-21 00:20:26 +0900

    feat: Header Footer 구현

--- apps/intra/tailwind.config.ts
+++ apps/intra/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     '../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   presets: [require('../../tailwind.config.js')],
   theme: {
