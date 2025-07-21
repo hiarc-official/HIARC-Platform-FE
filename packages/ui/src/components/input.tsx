0c79f0069c164ef2a826f09a3dd76626357ea377
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-21 13:44:42 +0900

    feat: Header Input컴포넌트 변경, 전체 ui input variant 추가

--- packages/ui/src/components/input.tsx
+++ packages/ui/src/components/input.tsx
@@ -1,19 +1,52 @@
 import * as React from 'react';
 import { cn } from '../lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-function Input({ className, type, ...props }: React.ComponentProps<'input'>): React.ReactElement {
+const inputVariants = cva(
+  cn(
+    'shadow-xs border-input selection:text-primary-foreground file:text-foreground placeholder:text-muted-foreground dark:bg-input/30 flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base outline-none transition-[color,box-shadow] selection:bg-primary file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+    'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+    'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
+  ),
+  {
+    variants: {
+      variant: {
+        search: cn('w-60 rounded-md bg-gray-100 px-3 py-2 pl-10 text-sm'),
+        default: '',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
+
+function Input({
+  className,
+  variant,
+  type,
+  ...props
+}: React.ComponentProps<'input'> & VariantProps<typeof inputVariants>): React.ReactElement {
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        'shadow-xs flex h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-base outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 dark:bg-input/30 md:text-sm',
-        'focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50',
-        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-        className
+    <div className="relative w-full">
+      {type === 'search' && (
+        /* eslint-disable @next/next/no-img-element */
+        /*img태그 성능이슈 있음 */
+        <img
+          src="ZoomIn.svg"
+          alt="Search Icon"
+          width={16}
+          height={16}
+          className="absolute left-3 top-1/2 -translate-y-1/2 opacity-60"
+        />
       )}
-      {...props}
-    />
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(inputVariants({ variant, className }))}
+        {...props}
+      />
+    </div>
   );
 }
 
