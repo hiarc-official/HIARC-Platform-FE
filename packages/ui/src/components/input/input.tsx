e1d348e0da97f43b27c46d07599182a9af69fcfd
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-22 03:26:47 +0900

    refactor: Header Css 변경

--- packages/ui/src/components/input/input.tsx
+++ packages/ui/src/components/input/input.tsx
@@ -6,7 +6,8 @@ const inputVariants = cva(
   cn(
     'shadow-xs border-input file:text-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base outline-none transition-[color,box-shadow] selection:bg-primary file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
     'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-    'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
+    'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+    'placeholder-gray-500'
   ),
   {
     variants: {
