53faebf473bbe8b5002e112f04f1feb16b10a494
NOGUEN <nhg1113@icloud.com>
2025-07-22 09:43:34 +0900

    fix: signup페이지 import 수정과 레이아웃 변경

--- apps/intra/src/app/signup/page.tsx
+++ apps/intra/src/app/signup/page.tsx
@@ -1,13 +1,24 @@
 'use client';
 
-import ToggleSelector from '@/ui/LabeledSelector';
+import { LabeledSelector } from '@hiarc-platform/ui';
+import { selectOption } from 'constants/selectOption';
 
 export default function SignUpPage(): React.ReactElement {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <ToggleSelector required={true} type="학년" placeholder="학년을 입력해주세요" />
-        <ToggleSelector type="학과" placeholder="학과을 입력해주세요" required={true} />
+      <div className="z-10 flex w-full max-w-[390px] flex-col items-center gap-8">
+        <LabeledSelector
+          required={true}
+          label="학년"
+          placeholder="학년을 입력해주세요"
+          options={selectOption['학년']}
+        />
+        <LabeledSelector
+          label="학과"
+          placeholder="학과을 입력해주세요"
+          required={true}
+          options={selectOption['학과']}
+        />
       </div>
     </main>
   );
