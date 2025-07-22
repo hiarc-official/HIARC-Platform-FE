07d2b373cf58ad107ec132e3f4d28fa721e21f8c
NOGUEN <nhg1113@icloud.com>
2025-07-22 10:00:30 +0900

    feat: 회원가입 페이지 입력 폼 일부 구현

--- apps/intra/src/app/signup/page.tsx
+++ apps/intra/src/app/signup/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 
-import { LabeledSelector } from '@hiarc-platform/ui';
+import { Button, LabeledInput, LabeledSelector, Title } from '@hiarc-platform/ui';
 import { selectOption } from 'constants/selectOption';
 
 export default function SignUpPage(): React.ReactElement {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 flex w-full max-w-[390px] flex-col items-center gap-8">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="z-10 flex w-full max-w-[390px] flex-col items-center gap-4">
+        <Title size="sm" weight="bold" className="mt-8">
+          회원가입
+        </Title>
+        <LabeledInput label="이름" required={true} placeholder="이름을 입력해주세요" />
+        <LabeledInput label="전화번호" required={true} placeholder="-없이 숫자만 입력해주세요" />
+        <LabeledInput label="학번" required={true} placeholder="학번을 입력해주세요" />
         <LabeledSelector
           required={true}
           label="학년"
@@ -19,6 +25,15 @@ export default function SignUpPage(): React.ReactElement {
           required={true}
           options={selectOption['학과']}
         />
+        <div className="flex w-full items-end gap-2">
+          <LabeledInput label="BOJ" required={true} placeholder="백준 핸들을 입력해주세요" />
+          <Button variant="fill" size="md">
+            인증하기
+          </Button>
+        </div>
+        <Button variant="fill" size="lg" className="w-full" type="submit">
+          회원가입
+        </Button>
       </div>
     </main>
   );
