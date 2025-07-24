7f10cf6b98e78e0b5f981266dac8b0ee1c7004e9
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-24 17:58:20 +0900

    feat: StudyCard 개발 #25

--- apps/intra/constants/level.ts
+++ apps/intra/constants/level.ts
@@ -9,3 +9,9 @@ export const LevelIcon: Record<Level, string> = {
   [Level.INTERMEDIATE]: '/Intermediate.svg',
   [Level.EXPERT]: '/Expert.svg',
 };
+
+export const KoreanLevel: Record<Level, string> = {
+  [Level.BASIC]: '기초',
+  [Level.INTERMEDIATE]: '초급',
+  [Level.EXPERT]: '중급',
+};
