29b60248935c67d91a0026acd85a120f32ab324e
NOGUEN <nhg1113@icloud.com>
2025-07-24 21:39:36 +0900

    feat: ImageProvider 등록
    - Image 컴포넌트를 받기 위한 ImageProvider 등록

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { ImageProvider } from '@hiarc-platform/ui';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { NextImageAdapter } from './shared/provider/next-image-adapter';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ImageProvider imageComponent={NextImageAdapter}>
+        <body className={inter.className}>{children}</body>
+      </ImageProvider>
     </html>
   );
 }
