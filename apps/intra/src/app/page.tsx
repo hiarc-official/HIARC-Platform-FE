c30078e87bf13911217d86709ec4698c24e1fc1a
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-26 16:01:45 +0900

    feat: 메인페이지 Layout

--- apps/intra/src/app/page.tsx
+++ apps/intra/src/app/page.tsx
@@ -1,25 +1,65 @@
-import { Button, Card, CardContent, CardHeader, CardTitle } from '@hiarc-platform/ui';
-import { formatDate } from '@hiarc-platform/util';
-
-export default function Home() {
+import { Title } from '@hiarc-platform/ui';
+import StudyCard from '@/ui/studyCard/StudyCard';
+import { Level } from 'constants/level';
+export default function Home(): React.ReactElement {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Admin Dashboard - {formatDate(new Date())}
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <Card className="w-[350px]">
-            <CardHeader>
-              <CardTitle>Admin Stats</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Button variant="line" size={'sm'} className="mt-4">
-                Manage Users
-              </Button>
-            </CardContent>
-          </Card>
-        </div>
+    <main className="n mt-8 flex min-h-screen  w-full  flex-col  px-[360px]">
+      {/*상단 임시로 높이 402px*/}
+      <div className="flex h-[402px] gap-4">
+        <section className="w-[50%]">
+          <Title size="sm" weight="bold" className="mb-2">
+            학회일정
+          </Title>
+        </section>
+        <section className="w-[50%]">
+          <Title size="sm" weight="bold" className="mb-2">
+            공지사항/알고리즘소식
+          </Title>
+        </section>
+      </div>
+
+      {/* 하단 */}
+      <div>
+        <section className="">
+          <Title size="sm" weight="bold" className="mb-2">
+            스터디목록
+          </Title>
+          <div className="grid grid-cols-2 gap-4 ">
+            <StudyCard
+              time="화,금 (8시)"
+              delivery="대면"
+              studyLevel={Level.BASIC}
+              studyTitle="초급스터디"
+              hostName="이가은"
+              startDate="2025.03.25"
+              endDate="2025.06.20"
+              studyDiscription="스터디 설명"
+              state="participating"
+            />
+            <StudyCard
+              time="화,금 (8시)"
+              delivery="대면"
+              studyLevel={Level.INTERMEDIATE}
+              studyTitle="초급스터디"
+              hostName="이태경"
+              startDate="2025.03.25"
+              endDate="2025.06.20"
+              studyDiscription="스터디 설명"
+              state="recruiting"
+            />
+            <StudyCard
+              time="화,금 (8시)"
+              delivery="비대면"
+              studyLevel={Level.EXPERT}
+              studyTitle="초급스터디"
+              hostName="송한서"
+              startDate="2025.03.25"
+              endDate="2025.06.20"
+              studyDiscription="스터디 설명"
+              state="participating"
+            />
+          </div>
+        </section>
       </div>
     </main>
   );
