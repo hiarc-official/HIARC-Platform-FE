09fe6bb9f9de5c445de4d50a0c7cca3f4b8f4866
NOGUEN <nhg1113@icloud.com>
2025-07-29 16:46:51 +0900

    fix: Footer에 구분선 추가
    - 상단 구분선이 추가되어있지 않아 구분선 추가

--- apps/intra/src/ui/Footer.tsx
+++ apps/intra/src/ui/Footer.tsx
@@ -1,9 +1,13 @@
 import { Divider, Label } from '@hiarc-platform/ui';
 import Image from 'next/image';
 
-export default function Footer(): React.ReactElement {
+interface FooterProps {
+  id?: string;
+}
+
+export default function Footer({ id }: FooterProps): React.ReactElement {
   return (
-    <footer className="w-full px-6 py-4 sm:px-10">
+    <footer className="w-full border-t border-gray-200 px-6 py-4 sm:px-10" id={id}>
       <div className="mx-auto flex w-full max-w-screen-xl flex-col items-center justify-between gap-4 sm:flex-row">
         <div className="flex items-center gap-2 sm:text-left">
           <Label size="lg" weight="bold">
@@ -13,7 +17,6 @@ export default function Footer(): React.ReactElement {
             홍익대학교 컴퓨터공학과 알고리즘 학회
           </Label>
         </div>
-
         <div className="flex flex-wrap items-center justify-center gap-3 text-gray-600">
           <div className="flex items-center gap-2">
             <Image src="/Instagram.svg" alt="Instagram Icon" width={16} height={16} />
