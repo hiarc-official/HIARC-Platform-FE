ba6d5feb6cf9c0913170606cde9442e4694fc8fe
NOGUEN <nhg1113@icloud.com>
2025-07-29 18:40:47 +0900

    fix: PageRouter -> AppRouter

--- apps/intra/src/app/notice/[id]/page.tsx
+++ apps/intra/src/app/notice/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { BackButton, Button, Divider, Label, Title } from '@hiarc-platform/ui';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function NoticeDetail(): React.ReactElement {
   const router = useRouter();
